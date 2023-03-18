e1e9864463bb04e9e77e00a094cd0d698fadb408
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-18 15:15:51 +0530

    Added: program to check the frequency of a word.

--- src/com/hashtable/HashTableMain.java
+++ src/com/hashtable/HashTableMain.java
@@ -5,5 +5,13 @@ public class HashTableMain {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the HashTable Program. ");
+        HashTable table = new HashTable(10);
+        String sentence = "To be or not to be";
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            table.put(word, 1);
+        }
+        int frequency = table.get("be");
+        System.out.println("Frequency of 'be': " + frequency);
     }
 }
